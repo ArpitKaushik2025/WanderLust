14a567f45587fe4a370213262aa4c97d30a2b7b2
Arpit Kaushik <arpitkaushikgithub@gmail.com>
2025-04-22 20:42:33 +0530

    Added Category in New Form

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
-const { ref } = require("joi");
+const { ref, required } = require("joi");
 const dlink =
   "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
 
@@ -38,6 +38,11 @@ const listingSchema = new Schema({
       type: [Number],
       required: true,
     },
+  },
+  category: {
+    type: String,
+    enum: ["Boats", "Domes", "Arctic", "Farms", "Camping", "Pools", "Castles", "Mountains"],
+    required: true,
   },
 });
 
