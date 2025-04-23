87fefdf2f2fa7a8a0863cff32d84c1214ae5f3d9
Arpit Kaushik <arpitkaushikgithub@gmail.com>
2025-04-23 21:59:16 +0530

    Added new options in category

--- models/listing.js
+++ models/listing.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
-const { ref, required } = require("joi");
-const dlink =
-  "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
 
 const listingSchema = new Schema({
   title: {
@@ -41,7 +38,17 @@ const listingSchema = new Schema({
   },
   category: {
     type: String,
-    enum: ["Arctic", "Boats", "Camping", "Castles", "Domes", "Farms", "Mountains", "Pools"],
+    enum: [
+      "Arctic",
+      "Beach",
+      "Boats",
+      "Camping",
+      "Castles",
+      "City",
+      "Farms",
+      "Mountains",
+      "Pools",
+    ],
     required: true,
   },
 });
