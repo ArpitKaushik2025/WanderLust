372c4ce03fc17322af2e9be924950fa946856404
Arpit Kaushik <arpitkaushikgithub@gmail.com>
2025-05-06 10:40:52 +0530

    Fixed bug in popup

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,19 +1,14 @@
-const delBtn = document.querySelector(".btn-popup-del");
-const logoutBtn = document.querySelector(".btn-popup-logout");
-
-const alert = document.querySelector(".popup-delete");
-const logoutAlert = document.querySelector(".popup-logout");
-
-const alertBtns = document.querySelectorAll(".btn-del");
-const logoutAlertBtns = document.querySelectorAll(".btn-logout");
-
-const body = document.querySelector("body");
-const myData = document.getElementById("my-data");
-
-console.log(delBtn);
+if (!window.myData) {
+  window.myData = document.getElementById("my-data");
+}
 
 // Delete & Logout Popup
-if (myData != null) {
+if (window.myData != null) {
+  const delBtn = document.querySelector(".btn-popup-del");
+  const alert = document.querySelector(".popup-delete");
+  const alertBtns = document.querySelectorAll(".btn-del");
+  const body = document.querySelector("body");
+
   delBtn.addEventListener("click", () => {
     alert.style.display = "inline";
     body.style.overflow = "hidden";
@@ -26,6 +21,11 @@ if (myData != null) {
     });
   }
 } else {
+  const logoutBtn = document.querySelector(".btn-popup-logout");
+  const logoutAlert = document.querySelector(".popup-logout");
+  const logoutAlertBtns = document.querySelectorAll(".btn-logout");
+  const body = document.querySelector("body");
+
   logoutBtn.addEventListener("click", () => {
     logoutAlert.style.display = "inline";
     body.style.overflow = "hidden";
