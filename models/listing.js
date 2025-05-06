22cac50e1302537308947b9f43043181af6a0201
Arpit Kaushik <arpitkaushikgithub@gmail.com>
2025-05-06 09:50:36 +0530

    Fixed bug in popup

--- models/listing.js
+++ models/listing.js
@@ -39,6 +39,7 @@ const listingSchema = new Schema({
   category: {
     type: String,
     enum: [
+      "N/A",
       "Arctic",
       "Beach",
       "Boats",
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,17 +1,40 @@
-const btn = document.querySelector(".btn-popup");
-const alert = document.querySelector(".popup-danger");
-const alertBtns = document.querySelectorAll(".btn-alert");
+const delBtn = document.querySelector(".btn-popup-del");
+const logoutBtn = document.querySelector(".btn-popup-logout");
+
+const alert = document.querySelector(".popup-delete");
+const logoutAlert = document.querySelector(".popup-logout");
+
+const alertBtns = document.querySelectorAll(".btn-del");
+const logoutAlertBtns = document.querySelectorAll(".btn-logout");
+
 const body = document.querySelector("body");
+const myData = document.getElementById("my-data");
+
+console.log(delBtn);
 
 // Delete & Logout Popup
-btn.addEventListener("click", () => {
-  alert.style.display = "inline";
-  body.style.overflow = "hidden";
-});
-
-for (eachBtn of alertBtns) {
-  eachBtn.addEventListener("click", () => {
-    alert.style.display = "none";
-    body.style.overflow = "auto";
+if (myData != null) {
+  delBtn.addEventListener("click", () => {
+    alert.style.display = "inline";
+    body.style.overflow = "hidden";
   });
+
+  for (eachBtn of alertBtns) {
+    eachBtn.addEventListener("click", () => {
+      alert.style.display = "none";
+      body.style.overflow = "auto";
+    });
+  }
+} else {
+  logoutBtn.addEventListener("click", () => {
+    logoutAlert.style.display = "inline";
+    body.style.overflow = "hidden";
+  });
+
+  for (eachBtn of logoutAlertBtns) {
+    eachBtn.addEventListener("click", () => {
+      logoutAlert.style.display = "none";
+      body.style.overflow = "auto";
+    });
+  }
 }
